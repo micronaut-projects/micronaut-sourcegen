757c68f52a2da026c70ece5ce611fe35b4cb47f7
issam liqali <liqaliissam@gmail.com>
2025-11-10 16:27:19 +0100

    update micronaut build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 includeBuild("build-logic")
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "8.0.0-M8"
+    id("io.micronaut.build.shared.settings") version "8.0.0-M9"
 }
 
 rootProject.name = "sourcegen-parent"
