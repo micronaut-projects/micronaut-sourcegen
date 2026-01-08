ce3bb422f62c88e8d553fca03054432ae2c4f21c
Sergio del Amo <sergio.delamo@softamo.com>
2026-01-08 11:49:33 +0100

    Micronaut Build gradle plugin 8.0.0-M13

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 includeBuild("build-logic")
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "7.6.6"
+    id("io.micronaut.build.shared.settings") version "8.0.0-M13"
 }
 
 rootProject.name = "sourcegen-parent"
